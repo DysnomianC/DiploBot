d530bf24b9622d5b2dbce09453c8f662c733e4b7
DysnomianC <Dysnomian.gamer@gmail.com>
2021-06-10 07:08:12 +1200

    Hiding sensitive info in config

--- DiploBot/Diplomacy/config.js
+++ DiploBot/Diplomacy/config.js
@@ -1,17 +1,17 @@
 export default {
 	is_debug: false,
 	bot: {
-		bot_secret: "NjkyMjQzOTQzMzM2NzA2MDc4.XnrwtA.vjQe06F0_HqcpF4ptl4jPqrala0",
+		bot_secret: "{Secret}",
 
-		pastorders_channel_id: "692201168264233051",
-		bot_commands_channel_id: "692200991612993608",
-		france_id: "692209375636488193",
-		italy_id: "692209409228537906",
-		ah_id: "692209469740023909",
-		turk_id: "692209508021436436",
-		rus_id: "692209540325703701",
-		germ_id: "692209585775312897",
-		eng_id: "692209632575356950",
+		pastorders_channel_id: "{Secret}",
+		bot_commands_channel_id: "{Secret}",
+		france_id: "{Secret}",
+		italy_id: "{Secret}",
+		ah_id: "{Secret}",
+		turk_id: "{Secret}",
+		rus_id: "{Secret}",
+		germ_id: "{Secret}",
+		eng_id: "{Secret}",
 
 		done_string: "done",
 		undone_string: "undone",
